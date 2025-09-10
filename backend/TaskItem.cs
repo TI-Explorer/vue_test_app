55a846ae74ba275ea002ca61a24e7fbba3cfa0ac
TI-Explorer <ethanhara@gmail.com>
2025-09-10 12:50:37 -0400

    updated how the status is displayed on the cards

--- backend/TaskItem.cs
+++ backend/TaskItem.cs
@@ -4,7 +4,7 @@ public class TaskItem
 {
     public int Id { get; set; }
     public string Title { get; set; } = string.Empty;
-    public string Status { get; set; } = "ToDo"; //ToDo, InProgress, Done
+    public int Status { get; set; } = 0; //ToDo, InProgress, Done
     public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
     public int Priority { get; set; } = 0; //low = 0 med = 1 high = 2 
 }
